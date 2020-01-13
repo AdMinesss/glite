012d237f92255c325c26407f10958c01abed26b6
AdMinesss <stokolos1997@gmail.com>
2020-01-13 18:16:08 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?
 function ava_cash($steam) {
-	$ava_reserv = 'you_reserv_avatar'
+	$ava_reserv = 'you_reserv_avatar';
 	$key = 'you_steam_api_code';
 	$str = substr("$steam", 8);
-	$pieces = explode(":", $str);
-	$account_id = (($pieces[1] * 2) + $pieces[0]);
+	$steamexp = explode(":", $str);
+	$account_id = (($steamexp[1] * 2) + $steamexp[0]);
 	$steamid_64 = $account_id + 76561197960265728;
 	$file = "path_to_the_cache_folder/{$steamid_64}.txt";
 	if((!file_exists ($file)) OR (filesize($file) == '0')) {
